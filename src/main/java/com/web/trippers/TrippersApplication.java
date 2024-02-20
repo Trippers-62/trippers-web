1daa804f317016a7de124ebb2d4f8437a781737c
woo-lala <hyunj2034@naver.com>
2024-02-20 14:30:00 +0900

    init project

---
+++ src/main/java/com/web/trippers/TrippersApplication.java
@@ -0,0 +1,13 @@
+package com.web.trippers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrippersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrippersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web/trippers/TrippersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web.trippers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrippersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
